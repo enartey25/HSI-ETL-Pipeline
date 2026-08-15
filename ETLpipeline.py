709c0b2dec6d6d9a7236abcf4f0eee6e26ea8cd6
Ethan Nartey <ethannartey2006@gmail.com>
2026-08-15 10:20:32 +0000

    Started loading step

--- ETLpipeline.py
+++ ETLpipeline.py
@@ -6,6 +6,16 @@ import pandas as pd
 import psycopg2
 pd.set_option('display.max_columns', 100)
 
+load_dotenv()
+
+conn = psycopg2.connect(
+    host = os.getenv('DB_HOST'),
+    port = os.getenv('DB_PORT'),
+    dbname = os.getenv('DB_NAME'),
+    user = os.getenv('DB_USER'),
+    password = os.getenv('DB_PASSWORD')
+)
+
 def extract(symbol):
     data = yf.download(
         symbol,
